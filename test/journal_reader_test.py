7f8fa8cb7ca199d287026c6461667d7e70271318
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-16 10:56:03 +0300

    refactor: fix test for journal_reader

--- test/journal_reader_test.py
+++ test/journal_reader_test.py
@@ -8,12 +8,12 @@ from src.journal_reader import main
 def test_journal_reader():
     result, isError = main()
 
-    assert result in not None, "[ERROR] Result can't be None"
+    assert result is not None, "[ERROR] Result can't be None"
     assert len(result) > 0, "[ERROR] Result can't be blank"
 
     if (isError):
-        assert "Permission denied" in result, "[ERROR] Permission denied"
-        assert "No such file" in result, "[ERROR] No such file or directory"
+        assert "Permission denied" not in result, "[ERROR] Permission denied"
+        assert "No such file" not in result, "[ERROR] No such file or directory"
 
 
     print("{{SUCCSESS}} daemon is ready")
