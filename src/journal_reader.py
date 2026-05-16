c9f2f4bb3807cbb9b2f5c4d7eb6ff640070cc1cf
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-16 10:17:57 +0300

    fead: update jounal reader

--- src/journal_reader.py
+++ src/journal_reader.py
@@ -1,9 +1,13 @@
 import subprocess
 
-result = subprocess.run(
-    'journalctl -n 50 | cat',
-    shell=True,
-    capture_output=True,
-    text=True
-)
-print(result.stdout)
+def main():
+    result = subprocess.run(
+        'journalctl -n 50 | cat',
+        shell=True,
+        capture_output=True,
+        text=True
+    )
+    print(result.stdout)
+
+if __name__ == "__main__":
+    main()
